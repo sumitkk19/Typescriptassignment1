7096980b985f195278782ce5174b62a32e04f6d2
sumitkk19 <65333951+sumitkk19@users.noreply.github.com>
2023-03-20 20:02:57 +0530

    Create assignment1a.js

---
+++ assignment1a.js
@@ -0,0 +1,12 @@
+function Max(no1, no2, no3) {
+    if (no1 > no2 && no1 > no3) {
+        console.log("maximum number is" + no1);
+    }
+    else if (no2 > no3 && no2 > no1) {
+        console.log("maximum number is" + no2);
+    }
+    else {
+        console.log("maximum number is" + no3);
+    }
+}
+Max(23, 89, 6);
